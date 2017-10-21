d914322a751ddbbc3307b46459e9377453870d4d
陳科旭 <syiu0415tw@gmail.com>
2017-10-22 02:53:31 +0800

    Create Problem1_C.c
    Multiples of 3 and 5
    Problem 1

---
+++ Problem1_C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int T; 
+    scanf("%d", &T);
+    for(int i = 0; i < T; i++){
+        int N; 
+        scanf("%d", &N);
+        
+        // Number of multiples of 3
+        long N_multi_3=(N-1)/3;
+        // Sum of all multiples of 3
+        long Sum_multi_3=3*(1+N_multi_3)*N_multi_3/2;
+        
+        // Number of multiples of 5
+        long N_multi_5=(N-1)/5;
+        // Sum of all multiples of 5
+        long Sum_multi_5=5*(1+N_multi_5)*N_multi_5/2;
+
+        // Number of multiples of 15
+        long N_multi_15=(N-1)/15;
+        // Sum of all multiples of 15
+        long Sum_multi_15=15*(1+N_multi_15)*N_multi_15/2;
+
+        // Print sum of all multiples of 3 or 5 below N
+        printf("%ld\n", Sum_multi_3+Sum_multi_5-Sum_multi_15);
+    }
+    return 0;
+}
