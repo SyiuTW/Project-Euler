b238ecc096c6b2e5f2600f1d4733db0c38f89226
陳科旭 <syiu0415tw@gmail.com>
2017-10-30 03:52:57 +0800

    Create Problem1_Python-2.py
    Multiples of 3 and 5
    Problem 1

---
+++ Problem1_Python-2.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+T = int(raw_input().strip())
+for i in xrange(T):
+    N = int(raw_input().strip())
+    
+    # Number of multiples of 3
+    N_multi_3 = (N-1)/3
+    # Sum of all multiples of 3
+    Sum_multi_3 = 3*(1+N_multi_3)*N_multi_3/2
+
+    # Number of multiples of 5
+    N_multi_5 = (N-1)/5
+    # Sum of all multiples of 5
+    Sum_multi_5 = 5*(1+N_multi_5)*N_multi_5/2
+
+    # Number of multiples of 15
+    N_multi_15 = (N-1)/15
+    # Sum of all multiples of 15
+    Sum_multi_15 = 15*(1+N_multi_15)*N_multi_15/2
+
+    # Print sum of all multiples of 3 or 5 below N
+    print Sum_multi_3+Sum_multi_5-Sum_multi_15
