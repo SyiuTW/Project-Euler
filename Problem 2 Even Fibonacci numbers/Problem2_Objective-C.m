ab1dc9d1e80b7943c75c3ec36ca144e40500ca67
陳科旭 <syiu0415tw@gmail.com>
2017-11-15 12:55:34 +0800

    Create Problem2_Objective-C.m
    Even Fibonacci numbers
    Problem 2

---
+++ Problem 2 Even Fibonacci numbers/Problem2_Objective-C.m
@@ -0,0 +1,31 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]){
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int T;
+    scanf("%d", &T);
+    
+    for(int i = 0; i < T; i++){
+        long N;
+        scanf("%ld",&N);
+        
+        long temp;
+        long sum = 0;
+        // Start with 1 and 2
+        long F1 = 1;
+        long F2 = 2;
+        
+        while(F2 < N){
+            // Sum of even-valued terms
+            if (F2 % 2 == 0)
+                sum += F2;
+            // Add previous two terms to generate new term
+            temp = F1+F2;
+            F1 = F2;
+            F2 = temp;
+        }
+        printf("%ld\n", sum);
+    }
+    [pool drain];
+    return 0;
+}
