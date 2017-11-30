1c9625c20286343788712edf6dd8cc0a30850c38
陳科旭 <syiu0415tw@gmail.com>
2017-11-30 13:01:23 +0800

    Update Problem3_D.d
    Add comments to improve readability

--- Problem 3 Largest prime factor/Problem3_D.d
+++ Problem 3 Largest prime factor/Problem3_D.d
@@ -10,17 +10,22 @@ void main() {
         
         long LargestPrime = 2;
         
+        // If N is multiple of 2, iterate process of division by 2 to factorize N
         while (N % 2 == 0){
             N /= 2;
         }
 
+        // Any prime number other than 2 are odd
         for(long j = 3; (j * j) <= N; j = j + 2){
+            // If N is multiple of j, iterate process of division by j to factorize N
             while (N % j == 0){
                 N /= j;
             }
+            // Update largest prime factor
             LargestPrime = j;
         }
 
+        // Update largest prime factor
         if (N > 1){
             LargestPrime = N;
         }
