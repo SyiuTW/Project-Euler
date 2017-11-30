c8008f972b32373d44fdefc3e28c4c85c78797d7
陳科旭 <syiu0415tw@gmail.com>
2017-11-30 12:52:18 +0800

    Update Problem3_C#.cs
    Add comments to improve readability

--- Problem 3 Largest prime factor/Problem3_C#.cs
+++ Problem 3 Largest prime factor/Problem3_C#.cs
@@ -7,17 +7,22 @@ class Problem3 {
             long N = Convert.ToInt64(Console.ReadLine());
             long LargestPrime = 2;
             
+            // If N is multiple of 2, iterate process of division by 2 to factorize N
             while (N % 2 == 0){
                 N /= 2;
             }
             
+            // Any prime number other than 2 are odd
             for(long j = 3; (j * j) <= N; j = j + 2){
+                // If N is multiple of j, iterate process of division by j to factorize N
                 while (N % j == 0){
                     N /= j;
                 }
+                // Update largest prime factor
                 LargestPrime = j;
             }
             
+            // Update largest prime factor
             if (N > 1){
                 LargestPrime = N;
             }
